9fa539f097d1885826c4b1c1c0f0752b11a8e674
Enow Mbi <benowmbi@yahoo.com>
2019-12-05 00:07:11 +0100

    Modify movie model specs

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:movie){ FactoryBot.create(:movie) }
+
+  describe "Validations" do 
+
+    it "must have a title" do 
+      movie.title =""
+      expect(movie).not_to be_valid
+    end
+
+    it "must have a number_in_stock" do 
+      movie.number_in_stock =""
+      expect(movie).not_to be_valid
+    end
+
+    it "number_in_stock must be a numerical value" do 
+      movie.number_in_stock = "fifty"
+      expect(movie).not_to be_valid
+    end
+
+    it "must have daily_rental_rate " do 
+      movie.daily_rental_rate =""
+      expect(movie).not_to be_valid
+    end
+
+    it "daily_rental_rate must be a decimal value" do 
+      movie.daily_rental_rate ="four point five"
+      expect(movie).not_to be_valid
+    end
+
+  end
+
+
+  describe "Associations" do 
+    it "belongs to a genre" do 
+      genre = FactoryBot.create(:genre)
+      movie = FactoryBot.build(:movie, genre: genre)
+      expect(movie.genre).to eq genre
+    end
+
+  end
+
+  describe "Graceful Deletion" do 
+    it "reduces movie count by 1" do 
+      movie = FactoryBot.create(:movie)
+      expect{movie.destroy}.to change(Movie,:count).by(-1)
+    end
+
+  end
+
 end
