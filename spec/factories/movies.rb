5b86715df012662fe0ccb72269b3bf165843b899
Enow Mbi <benowmbi@yahoo.com>
2019-12-05 00:07:27 +0100

    Modify movies factory

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :movie do
-    title { "MyString" }
-    genre { nil }
+    title { "The place" }
+    genre
     number_in_stock { 1 }
     daily_rental_rate { 1.5 }
   end
