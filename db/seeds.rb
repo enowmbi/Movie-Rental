fabbf38ed0cb5173137ed8bf34672aec00a5d3f6
Enow Mbi <benowmbi@yahoo.com>
2019-12-07 16:23:27 +0100

    Modify seeds.rb added code to create customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,6 @@ else
   #create movies in each genre
   Movie.create([{title: "The dressmaker",genre: Genre.first,number_in_stock: 10,daily_rental_rate: 4.5},{title: "Winter's war",genre: Genre.last,number_in_stock: 4,daily_rental_rate: 4}])
 
+  Customer.create([{name: "Jane Doe",phone: 123456789,email: "jane_doe@example.com"},{name: "John Doe",phone: "987461237",email: "john-doe@example.com"},{name:"Susan Ching",phone: "7887984755",email:"sussy@example.co.ch"}])
 end
+
