bcb8bff8e23247abae9874cd4a7e6eaa9d07d3fd
Enow Mbi <benowmbi@yahoo.com>
2019-12-07 13:41:59 +0100

    Modify customer model specs - added validations and graceful deletion

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:customer) { FactoryBot.create(:customer)}
+
+  describe "Validations" do 
+    it "must have a name" do 
+      customer.name = ""
+      expect(customer).not_to be_valid
+    end
+
+    it "must have a phone number" do 
+       customer.phone = ""
+       expect(customer).not_to be_valid
+    end
+
+    it "must have a correct phone number format" do 
+       customer.phone = "123456789"
+       expect(customer).to be_valid
+    end
+  
+    it "must have an email address of correct format" do 
+       customer.email = ""
+       expect(customer).not_to be_valid
+    end
+
+
+  end
+
+  describe "Graceful delete" do 
+     it "changes customer count by -1" do 
+       customer = FactoryBot.create(:customer)
+       expect{customer.destroy}.to change(Customer,:count).by(-1)
+     end
+  end
+
+
 end
