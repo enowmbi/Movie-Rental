a2497ed1a058f0626bc3966965e8c3c54bcb5b6f
Enow Mbi <benowmbi@yahoo.com>
2019-12-07 13:41:00 +0100

    Modify customer model - added validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,14 @@
 class Customer < ApplicationRecord
+
+  VALID_PHONE_NUMBER = /\d[0-9]\)*\z/
+  VALID_EMAIL_ADDRESS =/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  validates :name, presence: true
+  
+  validates :phone, presence: true, format: { with: VALID_PHONE_NUMBER,
+    message: "only positive numbers without spaces are allowed" }
+
+  validates :email, presence: true,uniqueness: true,format: {with: VALID_EMAIL_ADDRESS,
+             message: "only valid email addresses allowed"}
+
 end
