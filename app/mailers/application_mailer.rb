4296474a23de7d0ca9c4a2479cd40922ffe48a5c
Enow Mbi <benowmbi@yahoo.com>
2020-02-06 06:13:59 +0100

    Modify application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'from@example.com',subject: 'New subject',cc: 'admin@gcmega.net',bcc: 'info@divcon.net'
   layout 'mailer'
 end
